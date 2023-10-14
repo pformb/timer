07b0aa392492ce2527fcba32c14956ea2e5e401a
Paul Formby <paulformby@icloud.com>
2023-10-14 01:50:15 +0000

    added comments

--- timer1.js
+++ timer1.js
@@ -1,13 +1,13 @@
 const timer = () => {
-  for (let j = 2; j < process.argv.length; j++) {
-    if (process.argv[j] < 0 || process.argv[j] === NaN) {
+  for (let j = 2; j < process.argv.length; j++) { // loop through process.argv array
+    if (process.argv[j] < 0 || process.argv[j] === NaN) { // if = neg num or NaN, skip
       continue;
     }
-    let dingTime = process.argv[j] * 1000;
+    let dingTime = process.argv[j] * 1000; // dingTime = nums input into terminal x 1000 (ex 1 = 1000 ms)
     setTimeout(() => {
-      process.stdout.write("\x07");
+      process.stdout.write("\x07"); // timer set to ding every dingTime 
     }, dingTime);
   }
 };
 
-timer();
+timer(); // call timer 
